8af558a93c104232f5d213488f87686cdd113ef3
Rafael Almeida <rafaelalmeidatk@gmail.com>
2018-03-25 19:11:14 -0300

    Add frames reducer

---
+++ src/__tests__/frames_reducer.test.js
@@ -0,0 +1,147 @@
+import {List, Map, fromJS} from 'immutable';
+
+import reducer, {
+  newFrame,
+  setFrameSourceRect,
+  setFrameOffset,
+} from '../ducks/frames';
+
+describe('frames reducer', () => {
+  it('handles NEW_FRAME', () => {
+    const state = fromJS({
+      animations: [{
+        name: 'Hey!',
+        frames: [],
+        delay: 0,
+        repeat: false,
+      }]
+    });
+    const animationIndex = 0;
+    const action = newFrame(animationIndex);
+    const nextState = reducer(state, action);
+    expect(nextState).toEqual(fromJS({
+      animations: [{
+        name: 'Hey!',
+        frames: [{
+          sourceRect: {
+            x: 0,
+            y: 0,
+            width: 32,
+            height: 32,
+          },
+          colliders: [],
+          offset: {
+            x: 0,
+            y: 0,
+          },
+        }],
+        delay: 0,
+        repeat: false,
+      }]
+    }));
+  });
+
+  it('handles SET_FRAME_SOURCERECT', () => {
+    const state = fromJS({
+      animations: [{
+        name: 'Hey!',
+        frames: [{
+          sourceRect: {
+            x: 0,
+            y: 0,
+            width: 32,
+            height: 32,
+          },
+          colliders: [],
+          offset: {
+            x: 0,
+            y: 0,
+          },
+        }],
+        delay: 0,
+        repeat: false,
+      }]
+    });
+    const animationIndex = 0;
+    const frameIndex = 0;
+    const frameSourceRect = {
+      x: 96,
+      y: 96,
+      width: 64,
+      height: 64,
+    };
+    const action = setFrameSourceRect(animationIndex, frameIndex, frameSourceRect);
+    const nextState = reducer(state, action);
+    expect(nextState).toEqual(fromJS({
+      animations: [{
+        name: 'Hey!',
+        frames: [{
+          sourceRect: {
+            x: 96,
+            y: 96,
+            width: 64,
+            height: 64,
+          },
+          colliders: [],
+          offset: {
+            x: 0,
+            y: 0,
+          },
+        }],
+        delay: 0,
+        repeat: false,
+      }]
+    }));
+  });
+
+  it('handles SET_FRAME_OFFSET', () => {
+    const state = fromJS({
+      animations: [{
+        name: 'Hey!',
+        frames: [{
+          sourceRect: {
+            x: 0,
+            y: 0,
+            width: 32,
+            height: 32,
+          },
+          colliders: [],
+          offset: {
+            x: 0,
+            y: 0,
+          },
+        }],
+        delay: 0,
+        repeat: false,
+      }]
+    });
+    const animationIndex = 0;
+    const frameIndex = 0;
+    const frameOffset = {
+      x: 64,
+      y: 32,
+    };
+    const action = setFrameOffset(animationIndex, frameIndex, frameOffset);
+    const nextState = reducer(state, action);
+    expect(nextState).toEqual(fromJS({
+      animations: [{
+        name: 'Hey!',
+        frames: [{
+          sourceRect: {
+            x: 0,
+            y: 0,
+            width: 32,
+            height: 32,
+          },
+          colliders: [],
+          offset: {
+            x: 64,
+            y: 32,
+          },
+        }],
+        delay: 0,
+        repeat: false,
+      }]
+    }));
+  })
+})
