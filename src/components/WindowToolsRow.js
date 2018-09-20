e888b9ec9e2ac945d2403e93ac37f896f1b2c18c
Rafael Almeida <rafaelalmeidatk@gmail.com>
2018-09-20 00:48:14 -0300

    Fix WindowToolsRow styling

--- src/components/WindowToolsRow.js
+++ src/components/WindowToolsRow.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ButtonGroup, Button, Alignment, Icon } from '@blueprintjs/core';
+import { ButtonGroup, Button, Icon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 
 const WindowToolsRow = ({ onClick, visibility, visibilityValue }) => (
   <ul className="window-tools-row">
     <ButtonGroup fill>
       {visibility && (
-        <li className="window-tools-icon" onClick={onClick}>
-          <Button
-          >
+        <li className="window-tools-icon">
+          <Button onClick={onClick}>
             {visibilityValue ? (
               <Icon icon={IconNames.EYE_OPEN} size={14} />
             ) : (
