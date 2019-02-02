8a4c3b66652f646094576c4ece449fe7eb1c1b2c
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-02-01 23:15:58 -0200

    Add dropdown menu to File option on navbar

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -4,17 +4,39 @@ import {
   NavbarGroup,
   NavbarHeading,
   NavbarDivider,
+  Position,
+  Popover,
   Button,
+  Menu,
 } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import { NAVBAR_HEIGHT } from '../helpers/constants';
+
+const FileButton = () => (
+  <Popover
+    content={
+      <Menu>
+        <Menu.Item text="New File" label="Ctrl + N" />
+        <Menu.Item text="Open File..." label="Ctrl + O" />
+        <Menu.Divider />
+        <Menu.Item text="Save" label="Ctrl + S" />
+        <Menu.Item text="Save As..." label="Ctrl + Shift + S" />
+        <Menu.Divider />
+        <Menu.Item text="Exit" label="Ctrl + Q" />
+      </Menu>
+    }
+    position={Position.BOTTOM}
+  >
+    <Button icon={IconNames.DOCUMENT} text="File" minimal />
+  </Popover>
+);
 
 const AppNavbar = () => (
   <Navbar style={{ height: NAVBAR_HEIGHT }}>
     <NavbarGroup style={{ height: NAVBAR_HEIGHT }}>
       <NavbarHeading>Hitbox</NavbarHeading>
       <NavbarDivider />
-      <Button icon={IconNames.DOCUMENT} text="File" minimal />
+      <FileButton />
       <Button icon={IconNames.COG} text="Settings" minimal />
     </NavbarGroup>
   </Navbar>
